0ceec980b0506ced97c2815e7c39502c94f70e27
Vladislav <gogoha.hug.8172@gmail.com>
2024-08-25 13:13:16 +0300

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SuperUltraMegaClick
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static event EventHandler? IsClickOn;
+
+        public static void ClickOn()
+        {
+            IsClickOn?.Invoke(null,EventArgs.Empty);
+        }
+
+
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Threading;
+using Gma.System.MouseKeyHook;
+using KeyEventArgs = System.Windows.Forms.KeyEventArgs;
+
+using WindowsInput;
+
+namespace SuperUltraMegaClick
+{
+
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+        private IKeyboardMouseEvents? m_globalhook;
+
+        bool isOn = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer =  new DispatcherTimer();
+            timer.Interval += TimeSpan.FromSeconds(0.0001);
+            StartKeyHook();
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var sim = new InputSimulator();
+            sim.Mouse.LeftButtonClick();
+            sim.Mouse.LeftButtonClick();
+            sim.Mouse.LeftButtonClick();
+            sim.Mouse.LeftButtonClick();
+            sim.Mouse.LeftButtonClick();
+            sim.Mouse.LeftButtonClick();
+            sim.Mouse.LeftButtonClick();
+            sim.Mouse.LeftButtonClick();
+            sim.Mouse.LeftButtonClick();
+            sim.Mouse.LeftButtonClick();
+            sim.Mouse.LeftButtonClick();
+
+
+        }
+
+
+        private void StartKeyHook()
+        {
+            m_globalhook = Hook.GlobalEvents();
+            m_globalhook.KeyDown += GlobalHookKeyDown;
+        }
+        private void GlobalHookKeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == System.Windows.Forms.Keys.RControlKey) 
+            {
+                if (!isOn)
+                {
+                    timer.Tick += Timer_Tick;
+                    timer.Start();
+
+                }
+                else
+                {
+                    timer.Tick -= Timer_Tick;
+                    timer.Stop();
+
+                }
+                isOn = !isOn;
+            }
+
+
+        }
+
+
+    }
+}
