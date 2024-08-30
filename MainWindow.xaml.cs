60b94c60e535222781ed3cb1a6aca65d3f87fa52
zeystom <gogoha.hug.8172@gmail.com>
2024-08-30 03:00:39 +0300

    GUI, Speed Settings, New TImer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,64 +1,84 @@
 ﻿using System.Text;
 using System.Windows;
+using System;
 using System.Windows.Threading;
 using Gma.System.MouseKeyHook;
 using KeyEventArgs = System.Windows.Forms.KeyEventArgs;
-
+using System.Timers;
 using WindowsInput;
-
+using Timer = System.Timers.Timer;
 namespace SuperUltraMegaClick
 {
-
     public partial class MainWindow : Window
     {
-        private DispatcherTimer timer;
-        private IKeyboardMouseEvents? m_globalhook;
+        private Timer _timer;
+        private IKeyboardMouseEvents? _globalHook;
+        private InputSimulator _inputSimulator = new();
+        private bool _initialized = false;
+        private bool _isOn = false;
+        private int _multiCounter = 1;
 
-        bool isOn = false;
         public MainWindow()
         {
+            _timer = new Timer(1000);
+            _timer.Elapsed += TimerElapsed;
             InitializeComponent();
-
-            timer =  new DispatcherTimer();
-            timer.Interval += TimeSpan.FromSeconds(0.0001);
             StartKeyHook();
-
+            InitializeSettings();
         }
 
-        private void Timer_Tick(object sender, EventArgs e)
+        private void InitializeSettings()
         {
-            var sim = new InputSimulator();
-
+            _initialized = true;
         }
 
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            for (int i = 0; i < _multiCounter; i++)
+            {
+                _inputSimulator.Mouse.LeftButtonClick();
+            }
+        }
 
         private void StartKeyHook()
         {
-            m_globalhook = Hook.GlobalEvents();
-            m_globalhook.KeyDown += GlobalHookKeyDown;
+            _globalHook = Hook.GlobalEvents();
+            _globalHook.KeyDown += GlobalHookKeyDown;
         }
+
         private void GlobalHookKeyDown(object sender, KeyEventArgs e)
         {
-
-            if (e.KeyCode == System.Windows.Forms.Keys.RControlKey) 
+            if (e.KeyCode == System.Windows.Forms.Keys.RControlKey)
             {
-                if (!isOn)
+                if (!_isOn)
                 {
-                    timer.Tick += Timer_Tick;
-                    timer.Start();
-
+                    _timer.Start();
                 }
                 else
                 {
-                    timer.Tick -= Timer_Tick;
-                    timer.Stop();
-
+                    _timer.Stop();
                 }
-                isOn = !isOn;
+                _isOn = !_isOn;
             }
-
-
         }
 
+        private void SliderCountOnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Counter.Text = SliderCount.Value.ToString();
+            if (_initialized)
+            {
+                double intervalInSeconds = 1.0 / e.NewValue;
+                _timer.Interval = intervalInSeconds * 1000;
+            }
+        }
+
+        private void MultiSliderCountOnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MultiCount.Text = MultiSliderCount.Value.ToString();
+            if (_initialized)
+            {
+                _multiCounter = (int)e.NewValue;
+            }
+        }
     }
 }
