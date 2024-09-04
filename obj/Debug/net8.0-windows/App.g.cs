304b6cb4ddb99ac5084ba17815b73b1ef13ef3df
zeystom <gogoha.hug.8172@gmail.com>
2024-09-05 02:34:50 +0300

    fixed ui

--- obj/Debug/net8.0-windows/App.g.cs
+++ obj/Debug/net8.0-windows/App.g.cs
@@ -44,7 +44,7 @@ namespace SuperUltraMegaClick {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace SuperUltraMegaClick {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.3.0")]
         public static void Main() {
             SuperUltraMegaClick.App app = new SuperUltraMegaClick.App();
             app.InitializeComponent();
